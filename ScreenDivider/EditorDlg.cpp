f7bf11a5a883a2ba4613796264074bce9cf94c2b
Jun-ho, Song <jun9358@gmail.com>
2013-11-20 00:46:52 +0900

    Fix drawing bug
    Because of using CPaintDC, the virtual window not drawed in dialog. So I
    add getting DC with GetDC() function and draw rectangle.

--- ScreenDivider/EditorDlg.cpp
+++ ScreenDivider/EditorDlg.cpp
@@ -66,11 +66,11 @@ void CEditorDlg::OnMouseMove(UINT nFlags, CPoint point)
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
 	if(m_isDown)
 	{
-		InvalidateRect(NULL,TRUE);
-		UpdateWindow();
 		m_end = point;
-		CPaintDC dc(this);
-		dc.Rectangle(m_start.x,m_start.y,m_end.x,m_end.y);
+
+		CDC *pDC;
+		pDC = GetDC();
+		pDC->Rectangle(m_start.x, m_start.y, m_end.x, m_end.y);
 	}
 	CDialogEx::OnMouseMove(nFlags, point);
 }
